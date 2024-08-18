6264faee4368024f3925da7ba1ac96872f4e958c
Kabilan Sathiyamurthy <124272705+kabilansathiyamurthy@users.noreply.github.com>
2024-08-18 15:16:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pyqrcode
 import streamlit as st
 s1 = "upi://pay?pa=kabilansathiya12345-1@okhdfcbank&am="
-s2 = st.text_input("Enter the Amount", "Please enter")
+s2 = st.text_input("Enter the Amount")
 s3 = "&cu=INR"
 s = s1+s2+s3
 if st.button("Generate"):
